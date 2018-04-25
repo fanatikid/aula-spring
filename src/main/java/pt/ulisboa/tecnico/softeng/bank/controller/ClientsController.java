9684d9006f0ab2c383d9f64bda4ea14dff19c3c7
Fernando Costa Oliveira <f.oliveira@programmer.net>
2018-04-25 18:13:36 +0100

    add blank ClientsController.

---
+++ src/main/java/pt/ulisboa/tecnico/softeng/bank/controller/ClientsController.java
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.softeng.bank.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pt.ulisboa.tecnico.softeng.bank.domain.Bank;
+import pt.ulisboa.tecnico.softeng.bank.domain.Client;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
+@Controller
+@RequestMapping(value = "/banks/bank/{code}/clients")
+public class ClientsController {
+	private static Logger logger = LoggerFactory.getLogger(ClientsController.class);
+
+}
