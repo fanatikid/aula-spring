b2c7ad84e999bf5adb0f0c90f044da3ee045ff8b
Fernando Costa Oliveira <f.oliveira@programmer.net>
2018-04-25 20:14:42 +0100

    added support for client creation.

---
+++ src/main/java/pt/ulisboa/tecnico/softeng/bank/controller/ClientController.java
@@ -0,0 +1,62 @@
+package pt.ulisboa.tecnico.softeng.bank.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pt.ulisboa.tecnico.softeng.bank.domain.Bank;
+import pt.ulisboa.tecnico.softeng.bank.domain.Client;
+import pt.ulisboa.tecnico.softeng.bank.dto.BankDto;
+import pt.ulisboa.tecnico.softeng.bank.dto.ClientDto;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
+@Controller
+@RequestMapping(value = "/banks/bank/{code}/clients")
+public class ClientController {
+	private static Logger logger = LoggerFactory.getLogger(ClientController.class);
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String clientForm(Model model, @PathVariable String code) {
+		logger.info("clientForm bankCode:{}", code);
+
+		Bank bankData = Bank.getBankByCode(code);
+
+		if (bankData == null) {
+			model.addAttribute("error", "Error: it does not exist a bank with the code " + code);
+			model.addAttribute("bank", new BankDto());
+			model.addAttribute("banks", Bank.banks);
+			return "banks";
+		}
+
+		model.addAttribute("client", new ClientDto());
+		model.addAttribute("bank", bankData);
+		return "clients";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String clientSubmit(Model model, @PathVariable String code, @ModelAttribute ClientDto client) {
+		logger.info("clientSubmit bankCode:{}, clientName:{}", code, client.getName());
+
+		try {
+			Bank.getBankByCode(code).addClient(
+					new Client(
+							Bank.getBankByCode(code), 
+							Bank.getBankByCode(code).getCode() + Bank.getBankByCode(code).getClients().size(), 
+							client.getName(), 
+							client.getAge()));
+			
+		} catch (BankException be) {
+			model.addAttribute("error", "Error: it was not possible to create the client");
+			model.addAttribute("client", client);
+			model.addAttribute("bank", Bank.getBankByCode(code));
+			return "clients";
+		}
+
+		return "redirect:/banks/bank/" + code;
+}
+}
